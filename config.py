361cb46ac1b17f87a677af73490ca5ca6010d061
sumanthg201994 <sumanthg@thetrinitysoftware.com>
2025-05-21 01:31:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ Created on Mon May 12 19:00:36 2025
 @author: HP
 """
 
-MODEL_PATH = r"home/loan_priority/model\xgb_model.pkl"
+MODEL_PATH = r"home/loan_priority/model/xgb_model.pkl"
 FEATURES = 'data/features.json'
